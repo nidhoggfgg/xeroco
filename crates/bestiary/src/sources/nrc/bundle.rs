ee8fef1c983c2b167f0b8bd42ebecaaabf5354ca
nidhogg <nidhoggfgg@gmail.com>
2026-05-06 02:22:46 +0800

    Rehome bestiary NRC bundle assets

--- crates/bestiary/src/sources/nrc/bundle.rs
+++ crates/bestiary/src/sources/nrc/bundle.rs
@@ -2,8 +2,6 @@ use std::path::PathBuf;
 
 pub fn bundled_bundle_dir() -> PathBuf {
     PathBuf::from(env!("CARGO_MANIFEST_DIR"))
-        .join("..")
-        .join("..")
-        .join("data")
+        .join("resources")
         .join("nrc_pokemon_data_bundle")
 }
